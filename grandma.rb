1f784e6bd5eeecc8da13cd6b3f938651168f0a53
An Pham <anpham148@gmail.com>
2020-06-18 13:20:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,9 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
