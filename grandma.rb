be2ea9e10188ee9a717a59c9bb49df7a9909e1bf
An Pham <anpham148@gmail.com>
2020-06-18 13:22:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,11 +16,11 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-    "NO, NOT SINCE 1938!"
-  else
+  elsif !phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
